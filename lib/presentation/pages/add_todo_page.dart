e43026e6f60a5ddddbdf9b5c74c51ce07e9baa12
Muhammad Athallah <mhd.athallah@gmail.com>
2022-03-13 13:48:00 +0700

    Add Indonesia locale

--- lib/presentation/pages/add_todo_page.dart
+++ lib/presentation/pages/add_todo_page.dart
@@ -5,6 +5,7 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({Key? key}) : super(key: key);
@@ -32,41 +33,11 @@ class _AddTodoPageState extends State<AddTodoPage> {
   TextEditingController _dateController = TextEditingController();
   TextEditingController _timeController = TextEditingController();
 
-  Future<Null> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        initialDatePickerMode: DatePickerMode.day,
-        firstDate: DateTime(2015),
-        lastDate: DateTime(2101));
-    if (picked != null)
-      setState(() {
-        selectedDate = picked;
-        _dateController.text = DateFormat.yMd().format(selectedDate);
-      });
-  }
-
-  Future<Null> _selectTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: selectedTime,
-    );
-    if (picked != null)
-      setState(() {
-        selectedTime = picked;
-        _hour = selectedTime.hour.toString();
-        _minute = selectedTime.minute.toString();
-        _time = _hour + ' : ' + _minute;
-        _timeController.text = _time;
-        _timeController.text = formatDate(
-            DateTime(2019, 08, 1, selectedTime.hour, selectedTime.minute),
-            [hh, ':', nn, " ", am]).toString();
-      });
-  }
-
   @override
   void initState() {
-    _dateController.text = DateFormat.yMd().format(DateTime.now());
+    initializeDateFormatting();
+
+    _dateController.text = DateFormat.yMd('id').format(DateTime.now());
 
     _timeController.text = formatDate(
         DateTime(2022, 3, 13, DateTime.now().hour, DateTime.now().minute),
@@ -78,7 +49,7 @@ class _AddTodoPageState extends State<AddTodoPage> {
   Widget build(BuildContext context) {
     _height = MediaQuery.of(context).size.height;
     _width = MediaQuery.of(context).size.width;
-    dateTime = DateFormat.yMd().format(DateTime.now());
+    dateTime = DateFormat.yMd('id').format(DateTime.now());
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -89,7 +60,6 @@ class _AddTodoPageState extends State<AddTodoPage> {
         width: _width,
         height: _height,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             Column(
@@ -133,7 +103,45 @@ class _AddTodoPageState extends State<AddTodoPage> {
             Column(
               children: <Widget>[
                 Text(
-                  'Choose Time',
+                  'Choose Start Time',
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.5),
+                ),
+                InkWell(
+                  onTap: () {
+                    _selectTime(context);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(top: 30),
+                    width: _width / 1.7,
+                    height: _height / 9,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: Colors.grey[200]),
+                    child: TextFormField(
+                      style: TextStyle(fontSize: 40),
+                      textAlign: TextAlign.center,
+                      onSaved: (String? val) {
+                        _setTime = val;
+                      },
+                      enabled: false,
+                      keyboardType: TextInputType.text,
+                      controller: _timeController,
+                      decoration: InputDecoration(
+                          disabledBorder:
+                              UnderlineInputBorder(borderSide: BorderSide.none),
+                          // labelText: 'Time',
+                          contentPadding: EdgeInsets.all(5)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Text(
+                  'Choose End Time',
                   style: TextStyle(
                       fontStyle: FontStyle.italic,
                       fontWeight: FontWeight.w600,
@@ -173,4 +181,37 @@ class _AddTodoPageState extends State<AddTodoPage> {
       ),
     );
   }
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        initialDatePickerMode: DatePickerMode.day,
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2101));
+    if (picked != null)
+      setState(() {
+        selectedDate = picked;
+        _dateController.text = DateFormat.yMd('id').format(selectedDate);
+      });
+  }
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null)
+      setState(() {
+        selectedTime = picked;
+        _hour = selectedTime.hour.toString();
+        _minute = selectedTime.minute.toString();
+        _time = _hour + ' : ' + _minute;
+        _timeController.text = _time;
+        _timeController.text = formatDate(
+            DateTime(2022, 3, 13, selectedTime.hour, selectedTime.minute),
+            [hh, ':', nn, " ", am]).toString();
+      });
+  }
+
 }
